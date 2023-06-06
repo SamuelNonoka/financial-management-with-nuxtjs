80915cdaf07c9cc1bd80fb076773bc8335165aaf
SamuelNonoka <nonokapereira@gmail.com>
2023-06-05 21:59:13 -0300

    feat: adicionando sub-pages

---
+++ pages/a-empresa.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="bg-gray-100">
+      <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+        <nuxt-link to="/a-empresa">A empresa</nuxt-link>
+        <nuxt-link to="/servicos">Serviços</nuxt-link>
+        <nuxt-link to="/produtos">Produtos</nuxt-link>
+      </div>
+    </div>
+
+    <div>
+      <div class="container mx-auto">
+        <h1 class="text-lg font-bold">A empresa</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
+          necessitatibus sequi animi dolores accusantium natus optio unde
+          doloremque omnis rem, eligendi laboriosam mollitia vero sunt officia,
+          tempora ratione, laborum voluptatibus.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
